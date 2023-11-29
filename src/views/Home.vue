f01dc9dea10c05252c3a038bb51d738a4a48f192
DyaZix-Code <79577721+DyaZix-Code@users.noreply.github.com>
2023-11-30 00:06:23 +0100

    feature: backend health check

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,4 +4,22 @@
 
 <script setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import axios from "axios";
+
+  async function handleDeadBackend() {
+    let response = '';
+
+    try {
+      await axios.get(import.meta.env.VITE_API_URL + "/health").then((res) => {
+        response = res.data;
+      });
+    } catch (e) {
+    } finally {
+      if (response === "") {
+        alert("Backend de l'application introuvable. Essayez de redémarrer le serveur ou configurez l'url de l'api dans le .env.local");
+      }
+    }
+  }
+
+  handleDeadBackend();
 </script>
